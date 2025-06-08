3894f4ad749b7168d52b0e148f421eb4506bd2aa
YunhuiHwang <dinojelly323@naver.com>
2025-06-08 22:03:09 +0900

    2025.06.08.

---
+++ src/java/bj_2025_06/bj_1546.java
@@ -0,0 +1,36 @@
+package java.bj_2025_06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_1546 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(strtk.nextToken());
+
+        double score[] = new double[n];
+        double avg, max = 0,sum = 0;
+
+        strtk = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++) {
+            score[i] = Double.parseDouble(strtk.nextToken());
+
+            if(score[i]>max)
+                max = score[i];
+        }
+
+        for(int i=0;i<score.length;i++) {
+            score[i] = (score[i]/max)*100;
+            sum += score[i];
+        }
+        br.close();
+
+        avg = sum / n;
+
+        System.out.println(avg);
+    }
+}
