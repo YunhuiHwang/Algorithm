78c14142a2aea2ff6773f7c3f56221976e0fead3
YunhuiHwang <dinojelly323@naver.com>
2025-08-01 23:35:02 +0900

    2025.08.01.

---
+++ src/java/bj_2025_07/bj_10988.java
@@ -0,0 +1,23 @@
+package java.bj_2025_07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int len = s.length();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? 1 : 0);
+    }
+}
---
+++ src/java/bj_2025_07/bj_1157.java
@@ -0,0 +1,32 @@
+package java.bj_2025_07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class bj_1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().toUpperCase();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < input.length(); i++) {
+            count[input.charAt(i) - 'A']++;
+        }
+
+        int max = -1;
+        char result = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                result = (char) (i + 'A');
+            } else if (count[i] == max) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+    }
+}
